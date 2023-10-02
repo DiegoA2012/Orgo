87fafa78d4ba6c8eeaeb7eb4cec2bb12272a32ea
Teyeti <108320378+DiegoA2012@users.noreply.github.com>
2023-10-02 14:32:55 -0500

    Version 2

--- Orgo.py
+++ Orgo.py
@@ -1,21 +1,30 @@
+from tkinter import Tk, Label, Button, Radiobutton, IntVar, BooleanVar, Checkbutton, StringVar, OptionMenu
 
-# Import necessary modules from the Tkinter library for GUI
-from tkinter import Tk, Label, Button, Radiobutton, IntVar
-# Import os and shutil for file operations, time for time.sleep, and threading for running organize_files in a separate thread
 import os
 import shutil
 import time
 import threading
 
-# Define the main class for the Tkinter app
+
 class OrgoApp:
     def __init__(self, master):
-        self.master = master  # Main window for Tkinter
+        self.master = master
+        # Welcome message
+        self.welcome_label = Label(master, text="Hi, I'm Orgo. Click on the drop down menu to start")
+        self.welcome_label.pack()
+
+        # OptionMenu for running mode
+        self.run_mode_var = StringVar(master)
+        self.run_mode_var.set("Choose One")  # Default value
+        self.run_mode_option_menu = OptionMenu(master, self.run_mode_var, "Run Once", "Run Continuously")
+        self.run_mode_option_menu.pack()
+
+                 
         self.stop_flag = False  # Initialize flag to control stopping the organizer
         master.title("Orgo: The Automatic File Organizer")  # Set the title of the window
 
         # Create and pack a label prompting the user to choose a time interval
-        self.label = Label(master, text="Choose the time interval for automatic organization:")
+        self.label = Label(master, text="Choose time interval for Orgo:")
         self.label.pack()
 
         # Initialize a Tkinter variable to hold the interval choice and create radio buttons for each choice
@@ -37,11 +46,11 @@ class OrgoApp:
         self.stop_button = Button(master, text="Stop", command=self.stop_organizing)
         self.stop_button.pack()
 
-    # Method to start the file organizing process
+    
     def start_organizing(self):
         self.stop_flag = False  # Reset the stop flag when starting
         interval = self.interval_choice.get()  # Get the chosen interval from the radio buttons
-        print(f"Starting to organize files every {interval} seconds.")
+        print(f"Orgo is working hard every {interval} seconds.")
         
         # Run the organize_files function in a separate thread so that the GUI remains responsive
         thread = threading.Thread(target=self.run_organizer, args=(interval,))
@@ -50,7 +59,7 @@ class OrgoApp:
     # Method to stop the file organizing process
     def stop_organizing(self):
         self.stop_flag = True  # Set the stop flag to stop the while loop in run_organizer
-        print("Stopping the file organization.")
+        print("Orgo is going to bed.")
 
     # Method that runs the organize_files function in a loop with a sleep interval
     def run_organizer(self, interval):
@@ -62,7 +71,19 @@ class OrgoApp:
 def organize_files():
     source_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
     file_list = os.listdir(source_folder)
-    file_type_mapping = { # Image Files
+    file_type_mapping = { 
+    'txt': 'Text',
+    'doc': 'Documents',
+    'docx': 'Documents',
+    'odt': 'Documents',
+    'pdf': 'PDFs',
+    'rtf': 'Documents',
+    'exe': 'Applications',  # Windows applications
+    'app': 'Applications',  # macOS applications
+    'sh': 'Applications',   # Linux shell scripts
+    'deb': 'Applications',  # Debian packages
+    'rpm': 'Applications',  # Red Hat packages
+    # Image Files
     'jpg': 'Images',
     'jpeg': 'Images',
     'png': 'Images',
